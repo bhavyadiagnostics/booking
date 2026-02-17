3bf38cc8f3ad8a4f9a02a002da959f8a469a0bfb
bhavyadiagnostics <diagnosticsbhavya@gmail.com>
2026-02-17 17:48:05 +0530

    Create getHistory.js

---
+++ api/getHistory.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+    const url = process.env.SHEET_URL;
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send("Error fetching data");
+    }
+}
