aa92589403bd224ba3bf88d0bc6fc01966037ae9
bhavyadiagnostics <diagnosticsbhavya@gmail.com>
2026-02-17 19:17:24 +0530

    getHistory.js

--- api/getHistory.js
+++ api/getHistory.js
@@ -1,11 +1,9 @@
 export default async function handler(req, res) {
-    const url = HISTORY_SHEET_URL;
+    const url = process.env.FORM_RESPONSES1_SHEET_URL; 
     try {
         const response = await fetch(url);
         const data = await response.text();
         res.setHeader('Content-Type', 'text/plain');
         res.status(200).send(data);
-    } catch (e) {
-        res.status(500).send("Error fetching data");
-    }
+    } catch (e) { res.status(500).send("Error"); }
 }
